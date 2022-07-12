9253f9f6bf4cc43bead37cab6c9ab797dec8728f
Eron Alves <eron.alves@rocketmail.com>
2022-07-11 23:49:26 -0300

    Added imgUrl as a property in User model

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,13 +1,16 @@
 export class User {
+
     name:string;
     email: string;
     workEmail?: string;
     addedIn: Date;
+    imgUrl: string;
 
-    constructor(name: string, email:string, workEmail?:string) {
+    constructor(name: string, email:string, imgUrl: string, workEmail?:string) {
         this.name = name;
         this.email = email;
         this.addedIn = new Date();
+        this.imgUrl = imgUrl;
         if(workEmail) this.workEmail = workEmail;
     }
 }
